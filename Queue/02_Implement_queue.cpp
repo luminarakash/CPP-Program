a788b76f95a1724cec6a739ba7ec562b1f1c4f45
Akash Patel <117297200+luminarakash@users.noreply.github.com>
2024-06-02 23:44:04 +0530

    queue

---
+++ Queue/02_Implement_queue.cpp
@@ -0,0 +1,84 @@
+#include<iostream>
+#include<queue>
+#include<algorithm>
+
+using namespace std;
+
+/*class Queue {
+    
+    int* arr;
+    int qfront;
+    int rear; 
+    int size;
+    
+public:
+    Queue() {
+        size = 100001;
+        arr = new int[size];
+        qfront = 0;
+        rear = 0;
+    }
+
+   
+
+    bool isEmpty() {
+        if(qfront == rear) {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    void enqueue(int data) {
+        if(rear == size)
+            cout << "Queue is Full" << endl;
+        else
+        {
+            arr[rear] = data;
+            rear++;
+        }
+    }
+
+    int dequeue() {
+        if(qfront == rear) {
+            return -1;
+        }
+        else
+        {	int ans = arr[qfront];
+            arr[qfront] = -1;
+            qfront++;
+            if(qfront==rear){
+                qfront = 0;
+                rear = 0;
+            }
+         return ans;
+        }
+    }
+
+    int front() {
+        if(qfront == rear) {
+            return -1;
+        }
+        else
+        {
+            return arr[qfront];
+        }
+    }
+};*/
+
+int main(){
+    queue<int>q;
+    int n;
+    cin>>n;
+    for(int i=0;i<n;i++)
+    {
+        int x;
+        cin>>x;
+        q.push(x);
+    }
+   
+    cout<<q.front()<<endl;
+    return 0;
+}
